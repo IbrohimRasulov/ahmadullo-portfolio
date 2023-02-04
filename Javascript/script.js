5ac241fdca5bd7304a231c536abafb3c6913cf16
ahmadulloh <ahmadillo.web@gmail.com>
2023-02-04 12:53:00 +0500

    message

--- Javascript/script.js
+++ Javascript/script.js
@@ -16,3 +16,39 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+//bot token
+var telegram_bot_id = "6051629956:AAFv9SummgDQZPdmj17b642L-Qv4zkmEdhE"; // token'ni o'rniga Siz yaratgan Bot tokenini yozing
+//chat id
+var chat_id = 1264116968; // 1111'ni o'rniga habar borishi kerak bo'lgan joyni ID'sini yozing (Batafsil videoda)
+var u_name, email, message;
+var ready = function() {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Ismi: " + u_name + "\nEmail: " + email + "\nIzoh: " + message;
+};
+var sendtelegram = function() {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
